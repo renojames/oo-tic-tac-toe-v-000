941bb072378f7588bb3482d02d9d0efdadc78202
Reno Sorensen <renojames19@gmail.com>
2017-11-30 02:05:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ WIN_COMBINATIONS = [
   end
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
 end
