0afa4258b2e67cae08f91eff155f08ba98c44ba7
Reno Sorensen <renojames19@gmail.com>
2017-11-30 02:07:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,10 +40,11 @@ WIN_COMBINATIONS = [
   end
 
   def turn_count
-  turn = 0
-  @board.each do |position|
-    if position == "X" || position == "O"
-      turn += 1
+    turn = 0
+      @board.each do |position|
+      if position == "X" || position == "O"
+        turn += 1
+      end
     end
   end
 
